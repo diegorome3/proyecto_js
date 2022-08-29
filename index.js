2bc04c4f9981347f139b7c576fa521a0de0e40ec
diegorome3 <diego_drr@hotmail.com>
2022-08-29 00:35:12 -0300

    proyecto javascript

--- index.js
+++ index.js
@@ -1,14 +1,51 @@
 
-var nombre = prompt("Ingrese su nombre");
-var nombre_ingresado = nombre
-var espacio = " "
 
-while (nombre == ""){
-    console.log("Por favor ingrese su nombre");
-    alert("Por favor ingrese su Nombre");
+function solicitarNombre() {
+    alert("Bienvenido a Arcat Games 🎮")
+    let nombre = prompt("Ingrese su nombre");
+    while(nombre ===""){
     nombre = prompt("Ingrese su nombre");
-    nombre_ingresado = nombre
-
-} if (nombre_ingresado != ""){
-    alert("su nombre es"+ espacio + nombre_ingresado)
+    } 
 }
+
+function mostrarJuegos(){
+let juegos;
+do{
+juegos = parseInt(prompt("Que juego estas buscando? : \n1)Dead Island 2\n2)Fifa 23\n3)Demons Souls"))}while(juegos!=1 && juegos!=2 && juegos!=3)
+
+switch(juegos){
+    case 1:
+        return "Dead Island 2";
+    case 2:
+        return "Fifa 23";
+    case 3:
+        return "Demons Souls"
+}
+
+}
+
+function validarPrecio(juego){
+ if (juego==="Dead Island 2"){
+   return 16000;
+}
+else if(juego==="Fifa 23"){
+   return 13000;
+   }else if(juego==="Demons Souls"){
+    return 10000;
+   }
+}
+
+function cobrar(nombre,precio){
+alert("Usted compra el siguiente Juego:"+nombre+"\nPrecio:$"+precio)
+let pago = parseInt(prompt("Con cuanto pagas?"))
+if(pago> precio){
+    alert("Gracias ! Su vuelto es "+ (pago-precio))
+}else{
+    alert("No te alcanza !😢")
+}
+}
+
+solicitarNombre();
+let juegoNombre = mostrarJuegos();
+let precioJuego = validarPrecio(juegoNombre);
+cobrar(juegoNombre,precioJuego)
