15a156f150936cbc6035f8dfa258773751e8d44d
diegorome3 <diego_drr@hotmail.com>
2022-09-08 19:17:11 -0300

    proyecto js

--- index.js
+++ index.js
@@ -1,51 +1,112 @@
+class Clientes {
+    constructor(nombre, mail) {
+        this.nombre = nombre
+        this.mail = mail
+    }
+}
 
+let arrayClientes = [];
 
 function solicitarNombre() {
     alert("Bienvenido a Arcat Games 🎮")
     let nombre = prompt("Ingrese su nombre");
-    while(nombre ===""){
-    nombre = prompt("Ingrese su nombre");
-    } 
-}
+    let mail = prompt("Ingrese su mail");
+    while (nombre === "") {
+        nombre = prompt("Ingrese su nombre");
+    }
 
-function mostrarJuegos(){
-let juegos;
-do{
-juegos = parseInt(prompt("Que juego estas buscando? : \n1)Dead Island 2\n2)Fifa 23\n3)Demons Souls"))}while(juegos!=1 && juegos!=2 && juegos!=3)
 
-switch(juegos){
-    case 1:
-        return "Dead Island 2";
-    case 2:
-        return "Fifa 23";
-    case 3:
-        return "Demons Souls"
-}
-
-}
-
-function validarPrecio(juego){
- if (juego==="Dead Island 2"){
-   return 16000;
-}
-else if(juego==="Fifa 23"){
-   return 13000;
-   }else if(juego==="Demons Souls"){
-    return 10000;
-   }
-}
-
-function cobrar(nombre,precio){
-alert("Usted compra el siguiente Juego:"+nombre+"\nPrecio:$"+precio)
-let pago = parseInt(prompt("Con cuanto pagas?"))
-if(pago> precio){
-    alert("Gracias ! Su vuelto es "+ (pago-precio))
-}else{
-    alert("No te alcanza !😢")
-}
+    let nuevoCliente = new Clientes(nombre, mail);
+    arrayClientes.push(nuevoCliente);
+    console.log(arrayClientes);
 }
 
 solicitarNombre();
-let juegoNombre = mostrarJuegos();
-let precioJuego = validarPrecio(juegoNombre);
-cobrar(juegoNombre,precioJuego)
+
+const listadoClientes = arrayClientes.map(arrayClientes => arrayClientes.nombre )
+
+console.log(listadoClientes)
+
+let option;
+
+const juegos = [
+    {
+        id: 1,
+        nombre: "Dead island 2",
+        estado: "Nuevo",
+        precio: "16000"
+    },
+    {
+        id: 2,
+        nombre: "Fifa 23",
+        estado: "Nuevo",
+        precio: "13000"
+    },
+    {
+        id: 3,
+        nombre: "Demons souls",
+        estado: "Nuevo",
+        precio: "10000"
+    },
+    {
+        id: 4,
+        nombre: "Returnal",
+        estado: "Nuevo",
+        precio: "14000"
+    },
+];
+
+const carrito = [];
+
+
+
+console.log(juegos)
+
+while (option !== 1 && option !== 2 && option !== 3 && option !== 4) {
+    option = Number(prompt("Ingrese entre 1,2,3,4 u 0. Dependiendo del juego que elijas :\n1. Dead island 2\n2. Fifa 23\n3. Demons souls\n4. Returnal\n0. Salir"));
+
+    switch (option) {
+        case 1:
+            agregarAlCarrito(juegos[option - 1])
+            alert("Gracias por comprar")
+            break;
+        case 2:
+            agregarAlCarrito(juegos[option - 1])
+            alert("Gracias por comprar")
+            break;
+        case 3:
+            agregarAlCarrito(juegos[option - 1])
+            alert("Gracias por comprar")
+            break;
+        case 4:
+            agregarAlCarrito(juegos[option - 1])
+            alert("Gracias por comprar")
+            break;
+        case 0:
+        default:
+            alert("Gracias, vuelva pronto.")
+    }
+}
+
+const resultado = juegos.filter(juego => juego.id >= 4);
+
+
+
+function agregarAlCarrito(id) {
+    carrito.push({
+        id
+    });
+    console.log(carrito[0].id.nombre);
+    console.log(id);
+
+    console.log("Se agrego el juego correctamente con el id" + id.id + "y nombre" + id.nombre);
+    //return "Se agrego el juego correctamente con el id " + id;
+    carrito.forEach(carri => console.log("Se agrego el juego correctamente con el id" + carri.id.id + " y nombre " + carri.id.nombre ))
+}
+
+console.log(resultado);
+function getLastID() {
+    return juegos.length;
+}
+
+
