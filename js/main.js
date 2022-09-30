f16e816d7199d92647e0f839ed2fe93ade0eb613
diegorome3 <diego_drr@hotmail.com>
2022-09-29 21:47:13 -0300

    proyecto js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 let shop = document.getElementById('shop')
+let loader = document.getElementById('loader')
+let error = document.getElementById('error')
 
 /**
  * ? Operadores condicionales
  */
+
 let cesta = JSON.parse(localStorage.getItem("carrito")) || []
     /**
      * ! Crea el HTML y lo completa con los datos del JSON 
      **/
+
 let generateShop = () => {
     return (shop.innerHTML = shopItemsData
         .map((x) => {
@@ -40,7 +44,7 @@ let generateShop = () => {
 }
 
 
-generateShop()
+// generateShop()
     /**
      * ! Al oprimir el botón incrementa la cantidad y la introduce en la cesta 
      **/
@@ -104,3 +108,50 @@ let calculation = () => {
     cartIcon.innerHTML = cesta.map((x) => x.item).reduce((x, y) => x + y, 0)
 }
 calculation()
+
+/**
+ * ! a Partir de Aqui AJAX y FETCH
+ * ? Al finalizar esta otro comentario igual a este
+ **/
+
+ const URL = `js/data.json`
+
+ let gestionadorLoader = () => {
+     loader.innerHTML = `
+     <div class="d-flex align-items-center">
+         <strong>Loading...</strong>
+         <div class="spinner-border ms-auto" role="status" aria-hidden="true"></div>
+     </div>
+ `
+     setTimeout(() => {
+         loader.innerHTML = ``
+     }, 1500)
+ }
+ 
+ let errorGenerador = () => {
+     return error.innerHTML = `
+  <div>
+      <h1> Four </h1>
+      <h1> Oh! </h1>
+      <h1> Four </h1>
+      <p>No puedo encontrar el contenido</p>
+      <img  width="400" src="images/cheems.jpg" alt="">
+  </div>
+  `
+ }
+ 
+ 
+ let manejadorDeErrores = () => {
+     gestionadorLoader()
+     let shop = ""
+     fetch(URL)
+         .then((response) => response.json())
+         .then((data) => {
+             shopItemsData = data
+             generateShop()
+         })
+         .catch((error) => {
+             errorGenerador()
+         })
+ }
+ manejadorDeErrores()
